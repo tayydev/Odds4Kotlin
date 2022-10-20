7e789e2b7ac8cfaebcce2838d466db804da6199a
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2022-10-20 14:07:56 -0400

    updates

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -39,18 +39,26 @@ data class Hand(private var cards: List<Card>): Comparable<Hand> {
         //todo find best hand
     }
 
-    fun hierarchy(): Hierarchy {
-        if(isFlush() && isStraight()) return Hierarchy.StraightFlush
-        if(isNPair(4)) return Hierarchy.Quads
-        if(isNPair(3) && isNPair(2)) return Hierarchy.FullHouse
-        if(isFlush()) return Hierarchy.Flush
-        if(isStraight()) return Hierarchy.Straight
-        if(isNPair(3)) return Hierarchy.Trips
-        if(isTwoPair()) return Hierarchy.TwoPair
-        if(isNPair(2)) return Hierarchy.Pair
-        return Hierarchy.HighCard
+    fun hierarchy(): Pair<Hierarchy, List<Card>> {
+        if(isFlush() && isStraight()) return Hierarchy.StraightFlush to
+                cards.setHelper()
+        if(isNPair(4)) return Hierarchy.Quads to
+                cards.setHelper(4).plus(cards.setHelper(1))
+        if(isNPair(3) && isNPair(2)) return Hierarchy.FullHouse to
+                cards.setHelper(3).plus(cards.setHelper(2))
+        if(isFlush()) return Hierarchy.Flush to
+                cards.setHelper()
+        if(isStraight()) return Hierarchy.Straight to
+                cards.setHelper()
+        if(isNPair(3)) return Hierarchy.Trips to
+                cards.setHelper(3).plus(cards.setHelper(1))
+        if(isTwoPair()) return Hierarchy.TwoPair to
+                cards.setHelper(2).plus(cards.setHelper(1))
+        if(isNPair(2)) return Hierarchy.Pair to
+                cards.setHelper(2).plus(cards.setHelper(1))
+        return Hierarchy.HighCard to
+                cards.setHelper()
     }
-
     fun isFlush(): Boolean {
         return cards.all { it.suit == cards.first().suit } //assumes one card
     }
@@ -71,34 +79,10 @@ data class Hand(private var cards: List<Card>): Comparable<Hand> {
     }
 
     override fun compareTo(other: Hand): Int {
-        if(hierarchy() != other.hierarchy()) {
-            return hierarchy().compareTo(other.hierarchy())
-        }
-        when(hierarchy()) {
-            Hierarchy.StraightFlush -> return valueCompare(this.cards, other.cards)
-            Hierarchy.Quads -> {
-                val grouped = this.cards.groupBy { it.value }
-                val otherGrouped = other.cards.groupBy { it.value }
-
-                return priorityCompare(
-                    grouped.firstNotNullOf { it.value.size == 4 }.compareTo(otherGrouped.firstNotNullOf { it.value.size == 4 }),
-                    grouped.firstNotNullOf { it.value.size == 1 }.compareTo(otherGrouped.firstNotNullOf { it.value.size == 1 })
-                )
-            }
-            else -> {}
-        }
-        return -1
+        val first = hierarchy().first.compareTo(other.hierarchy().first)
+        val rest = hierarchy().second.zip(other.hierarchy().second).map { (self, other) -> self.compareTo(other) }
+        return priorityCompare(listOf(first).plus(rest))
     }
-
-    private fun valueCompare(self: List<Card>, other: List<Card>): Int {
-        return priorityCompare(
-            self.sortedDescending().zip(other.sortedDescending()).map { (self, other) ->
-                self.compareTo(other)
-            }
-        )
-    }
-
-    private fun priorityCompare(vararg compared: Int): Int = priorityCompare(compared.toList())
     private fun priorityCompare(compared: List<Int>): Int {
         return compared.firstOrNull { it != 0 } ?: 0
     }
@@ -143,4 +127,13 @@ enum class Value {
     Queen,
     King,
     Ace
+}
+
+//finds sets with n values and sorts descending
+private fun List<Card>.setHelper(num: Int = 1): List<Card> {
+    val sets = this.groupBy { it.value }
+        .filter {
+            it.value.size == num
+        }
+    return sets.map { it.value.first() }.sortedDescending()
 }
--- src/test/kotlin/HandTest.kt
+++ src/test/kotlin/HandTest.kt
@@ -8,27 +8,27 @@ internal class HandTest {
     @Test
     fun hierarchy() {
         val sfHand = Hand("AS 2S 3S 4S 5S")
-        assertEquals(sfHand.hierarchy(), Hierarchy.StraightFlush)
+        assertEquals(sfHand.hierarchy().first, Hierarchy.StraightFlush)
 
         val qHand = Hand("AD AS AC AH 2H")
-        assertEquals(qHand.hierarchy(), Hierarchy.Quads)
+        assertEquals(qHand.hierarchy().first, Hierarchy.Quads)
 
         val fhHand = Hand("AS AD AH JH JD")
-        assertEquals(fhHand.hierarchy(), Hierarchy.FullHouse)
+        assertEquals(fhHand.hierarchy().first, Hierarchy.FullHouse)
 
         val fHand = Hand("AS 4S 2S 9S 3S")
-        assertEquals(fHand.hierarchy(), Hierarchy.Flush)
+        assertEquals(fHand.hierarchy().first, Hierarchy.Flush)
 
         val sHand = Hand("AS 2D 3D 4D 5D")
-        assertEquals(sHand.hierarchy(), Hierarchy.Straight)
+        assertEquals(sHand.hierarchy().first, Hierarchy.Straight)
 
         val tHand = Hand("7S 7D 7H 2S 3H")
-        assertEquals(tHand.hierarchy(), Hierarchy.Trips)
+        assertEquals(tHand.hierarchy().first, Hierarchy.Trips)
 
         val tpHand = Hand("AS 2D AH JH JD")
-        assertEquals(tpHand.hierarchy(), Hierarchy.TwoPair)
+        assertEquals(tpHand.hierarchy().first, Hierarchy.TwoPair)
 
         val pHand = Hand("AS AD 2D 5H 8C 9S")
-        assertEquals(pHand.hierarchy(), Hierarchy.Pair)
+        assertEquals(pHand.hierarchy().first, Hierarchy.Pair)
     }
 }
